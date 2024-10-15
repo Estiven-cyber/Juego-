b4d21848d813702271cf885b2ef7a12b42b91ae0
Estiven-cyber <estivenpuentes322@gmail.com>
2024-10-15 16:11:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const myQuestAPI = {
     system: {
-        inactive: "myQuestAPI is inactive", // Se mostrará si MyQuestAPI no está activa.
-        enable: true, // Establezca en verdadero para habilitar y falso para deshabilitar
-		tag: "op" // etiqueta quién puede ver las misiones cuando el sistema[enable = false] ||  /etiqueta @s agregar op
+        inactive: "myQuestAPI is inactive", 
+        enable: true, 
+        tag: "op" 
     },
     getIdentifier: {
-        author: "§5MenGamer", // Nombre del Autor 
-        title: "§lMisiones Tutorial", // Titulo
-        description: "Termina las Misiones" // Descripción
-        },
+        author: "§5MenGamer", 
+        title: "§lMisiones apocalipticas", 
+        description: "Termina las Misiones" 
+    },
     getQuest: {
-    	isNamespace: { 
-			isDefault: false,  // Inútil por ahora.  Si es verdadero, usará el espacio de nombres predeterminado
-    							 	 // entonces ya no necesitas escribir espacio de nombres
-			namespace: "minecraft:" // Sólo funciona si "isDefault: true"
-		},
+        isNamespace: { 
+            isDefault: false,  
+            namespace: "minecraft:"
+        },
         itemHand: "minecraft:book",
         items: [
             "minecraft:coal",
@@ -23,62 +22,77 @@ const myQuestAPI = {
             "minecraft:gold_ingot",
             "minecraft:diamond",
             "minecraft:netherite_ingot",
-            "minecraft:stone_pickaxe"
+            "minecraft:stone_pickaxe",
+            "minecraft:obsidian",
+            "minecraft:ender_pearl",
+            "minecraft:nether_star"
         ],
         rewards: [
-        	["minecraft:apple", 1],
-        	["minecraft:carrot", 1],
-        	["minecraft:potato", 1],
-        	["minecraft:sugar_cane", 1],
-        	["minecraft:wheat", 1],
-        	["minecraft:potion", 1],
-        	["minecraft:stone", 10]
+            ["minecraft:apple", 1],
+            ["minecraft:carrot", 1],
+            ["minecraft:potato", 1],
+            ["minecraft:sugar_cane", 1],
+            ["minecraft:wheat", 1],
+            ["minecraft:potion", 1],
+            ["minecraft:stone", 10],
+            ["minecraft:golden_apple", 1],
+            ["minecraft:diamond_sword", 1],
+            ["minecraft:elytra", 1]
         ]
     },
     getForm: {
-    	title: {
-    		enable: true, // Cambie a falso para generar el título automáticamente
-    		title: [
-    			// no es necesario completar este campo si el título[enable = true]
-    			"Title here",
-    			"Title here",
-    			"Title here",
-    			"Title here",
-    			"Title here",
-    			"Title here"
-    		]
-    	},
-    	description: {
-    		description: [
-    			"Colecciona Carbón",
-    			"Colecciona Hierro",
-    			"Colecciona Cobre",
-    			" ", // Puedes omitir la descripción escribiendo un espacio.
-    			"Colecciona diamantes",
-    			"Colecciona Netherita",
-    			"Has tu primer pico"
-    		],
-    		complated: "Completado",
-    		notComplated: "En Progreso"
-    	},
-    	icon: {
-    		enable: true, // Establezca en verdadero para habilitar y falso para deshabilitar
-    		confirm: `textures/ui/confirm`,
-    		items: [
-    			"items/coal",
-    			"items/iron_ingot",
-    			"items/copper_ingot",
-    			"items/gold_ingot",
-    			"items/diamond",
-    			"items/netherite_ingot",
-    			"items/stone_pickaxe"
-			]
-    	},
-    	button: {
-        	back: "§l§4Salir", // Boton Salir
-        	check: "§l§qProbar", // Boton check
-        	about: "§l§6Acerca De..."  // Boton AcercaDe...
-    	}
+        title: {
+            enable: true, 
+            title: [
+                "Colecciona Carbón",
+                "Colecciona Lingotes de Hierro",
+                "Colecciona Lingotes de Cobre",
+                "Colecciona Lingotes de Oro",
+                "Colecciona Diamantes",
+                "Colecciona Netherita",
+                "Haz tu primer Pico de Piedra",
+                "Colecciona Obsidiana",
+                "Encuentra una Perla de Ender",
+                "Consigue una Estrella del Nether"
+            ]
+        },
+        description: {
+            description: [
+                "Colecciona 10 Carbones",
+                "Colecciona 10 Lingotes de Hierro",
+                "Colecciona 10 Lingotes de Cobre",
+                "Colecciona 10 Lingotes de Oro",
+                "Colecciona 5 Diamantes",
+                "Colecciona 1 Netherita",
+                "Crea tu primer Pico de Piedra",
+                "Colecciona 5 Bloques de Obsidiana",
+                "Colecciona 1 Perla de Ender",
+                "Consigue una Estrella del Nether"
+            ],
+            complated: "Completado",
+            notComplated: "En Progreso"
+        },
+        icon: {
+            enable: true, 
+            confirm: `textures/ui/confirm`,
+            items: [
+                "items/coal",
+                "items/iron_ingot",
+                "items/copper_ingot",
+                "items/gold_ingot",
+                "items/diamond",
+                "items/netherite_ingot",
+                "items/stone_pickaxe",
+                "items/obsidian",
+                "items/ender_pearl",
+                "items/nether_star"
+            ]
+        },
+        button: {
+            back: "§l§4Salir", 
+            check: "§l§qProbar", 
+            about: "§l§6Acerca De..." 
+        }
     }
 }
 
